02c2a181a18df41488ab56aeb01cc0b0a499294f
Chris Hidey <chidey@cs.columbia.edu>
2016-03-03 20:00:42 -0500

    script for identifying POTENTIAL altlexes based on rules and alignments

---
+++ misc/alignAltlexes.py
@@ -0,0 +1,22 @@
+import sys
+
+from altlex.utils import wordUtils
+from altlex.utils.readers.alignedParsedPairIterator import AlignedParsedPairIterator
+
+if __name__ == '__main__':
+    parallelParseDir = sys.argv[1]
+    print('loading alignments...')
+    with open(sys.argv[2]) as f:
+        alignments = f.read().splitlines()
+    prefix = sys.argv[3]
+    initLabelsFile = prefix + '_initLabels.json.gz'
+
+    if sys.argv[4] == '0':
+        seedSet, labelLookup = wordUtils.binaryCausalSettings
+    else:
+        seedSet, labelLookup = wordUtils.trinaryCausalSettings
+        
+    alignedLabels = AlignedParsedPairIterator(parallelParseDir, alignments)
+    alignedLabels.makeLabels(seedSet, labelLookup)
+    alignedLabels.save(initLabelsFile)
+    
