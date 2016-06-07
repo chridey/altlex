9feef96fdaef21c557fe1babe53ba8f014060bad
Chris Hidey <chidey@cs.columbia.edu>
2016-06-07 15:47:34 -0400

    updating wikipedia file to remove unneeded code and dependencies
    	modified:   wiknet/wikipedia.py

--- wiknet/wikipedia.py
+++ wiknet/wikipedia.py
@@ -9,9 +9,6 @@ import sys
 
 import nltk
 
-import requests
-import bs4
-
 bracketRe = re.compile('(.*?)(\[[^\[]*?\]|\Z)')
 stopwords = set(nltk.corpus.stopwords.words('english'))
 sn = nltk.stem.SnowballStemmer('english')
@@ -31,7 +28,7 @@ def cache(func):
     
     return wrapper
 
-def loadDiscourse(file='/home/chidey/PDTB/chnlp/config/markers', format=1):
+def loadDiscourse(file, format=1):
     with open(file) as f:
         if format == 1:
             return {tuple(getUnigrams(i)) for i in f.read().splitlines()}
@@ -54,53 +51,6 @@ def loadDiscourse(file='/home/chidey/PDTB/chnlp/config/markers', format=1):
             return ret                
         #return set(f.read().splitlines())
 
-def splitOnDiscourse(sentence, discourse, allConnectives=False):
-    unigrams = getUnigrams(sentence)
-    words = {length: zip(*(unigrams[i:] for i in range(length))) for length in range(1, max(map(len,discourse))+1)}
-    ngrams = set(itertools.chain(*words.values()))
-    relations = ngrams & discourse
-
-    if allConnectives:
-        return relations
-    
-    if len(relations):        
-        #if there is more than one, take the most centered one
-        relation, length, distance = min(((relation, length, abs(len(words)/2.-words[length].index(relation)))
-                                          for length,matches in itertools.groupby(relations, len)
-                                          for relation in matches),
-                                         key = lambda x:x[2])
-        #print(relation, length, distance, len(words)/2.)
-        index = words[length].index(relation)
-        splitSentence = nltk.word_tokenize(sentence)
-        return (splitSentence[:index],
-                splitSentence[index:index+len(relation)],
-                splitSentence[index+len(relation):])
-    return None
-
-def getParallelArticles(title, discourse=None):
-    corpora = []
-    for wiki in ('en', 'simple'):
-        print(wiki)
-        r = requests.get('https://{}.wikipedia.org/wiki/{}'.format(wiki, title))
-        soup = bs4.BeautifulSoup(r.text)#, 'lxml')
-        sentences = []
-        for paragraph in soup.find_all('p'):
-            if '.' in paragraph.text:
-                m = [i for i in bracketRe.finditer(paragraph.text)]
-                p = ' '.join(zip(*(i.groups() for i in m))[0])
-                #sentences += nltk.sent_tokenize(p)
-                #split sentences on discourse relations
-                for sent in nltk.sent_tokenize(p):
-                    if discourse:
-                        clauses = splitOnDiscourse(sent, discourse)
-                        if clauses is not None:
-                            sentences.extend([' '.join(clauses[0]),
-                                              ' '.join(clauses[1]+clauses[2])])
-                            continue
-                    sentences.append(sent)
-        corpora.append(sentences)
-    return corpora
-
 @cache
 def getUnigrams(sentence):
     return [sn.stem(i).lower() for i in nltk.word_tokenize(sentence)]
@@ -117,39 +67,6 @@ def getBagOfNgrams(sentence, filter=True):
 
     return words
 
-def getMatching(set1, set2):
-    #do jaccard index or whatever similarity metric
-
-    scores = {}
-    sets1 = []
-    sets2 = []
-    for sent1 in set1:
-        sets1.append(getBagOfNgrams(sent1))
-    for sent2 in set2:
-        sets2.append(getBagOfNgrams(sent2))
-
-    sets1.extend([i|j for i,j in zip(sets1, sets1[1:])])
-    sets2.extend([i|j for i,j in zip(sets2, sets2[1:])])
-
-    start1 = 0 #len(set1)
-    start2 = 0 #len(set2)
-    for i1, sent1 in enumerate(sets1[start1:]):
-        if not len(sent1):
-            continue
-        for i2, sent2 in enumerate(sets2[start2:]):
-            if not len(sent2):
-                continue
-            #for now do jaccard similarity
-            scores[(i1+start1,i2+start2)] = 1.*len(sent1 & sent2)/len(sent1 | sent2)
-
-    #for i1, sent1 in enumerate(sets1):
-    #    for i2, sent2 in enumerate(sets2):
-    #        scores[(i1,i2)] = 1.*len(sent1 & sent2)/len(sent1 | sent2)
-
-    #TODO: what about consecutive sentences where one contains a discourse marker?
-            
-    return scores
-
 def normalizeIndex(index, s):
     if index >= len(s):
         i0 = index - len(s)
@@ -159,11 +76,6 @@ def normalizeIndex(index, s):
         i1 = None
 
     return i0,i1
-
-def reverseIndex(i0, i1, s):
-    if i1 is None:
-        return i0
-    return i0 + len(s)
 
 def getMultiSentence(s, index1, index2):
     multisentence = s[index1]
@@ -180,59 +92,6 @@ def printPairs(set1, set2, scores, k=10, handle=sys.stdout):
 
         print(score)
 
-def getMaximalDPMatching(set1, set2, scores):
-    maxes = scores.copy() #np.zeros(shape=(len(set1), len(set2)))
-    argmaxes = {}
-
-    for combo in range(len(set1)+len(set2)-2):
-        for i in range(combo+1):
-            print(combo, i)
-            if i >= len(set1):
-                continue
-            j = combo-i
-            if j >= len(set2):
-                continue
-            if j > 0 and maxes[i,j-1] > maxes[i,j]:
-                maxes[i,j] = maxes[i,j-1]
-                argmaxes[(i,j)] = (i, j-1)
-            elif i > 0 and maxes[i-1, j] > maxes[i,j]:
-                maxes[i,j] = maxes[i-1, j]
-                argmaxes[(i,j)] = (i-1, j)
-            elif i > 0 and j > 0 and maxes[i-1, j-1] + scores[(i,j)] > maxes[i,j]:
-                maxes[i,j] = maxes[i-1, j-1] + scores[(i,j)]
-                argmaxes[(i,j)] = (i-1, j-1)
-            elif i > 0 and j > 1 and maxes[i-1, j-2] + scores[(i, reverseIndex(j-1, j, set1))] > maxes[i,j]:
-                maxes[i,j] = maxes[i-1, j-2] + scores[(i, reverseIndex(j-1, j, set2))]
-                argmaxes[(i,j)] = (i-1, j-2)
-            elif i > 1 and j > 0 and maxes[i-2, j-1] + scores[(reverseIndex(i-1, i, set1), j)] > maxes[i,j]:
-                maxes[i,j] = maxes[i-2, j-1] + scores[(reverseIndex(i-1, i, set1), j)]
-                argmaxes[(i,j)] = (i-2, j-1)
-            print(maxes[i,j], argmaxes[(i,j)])
-            
-    i,j = len(set1)-1,len(set2)-1
-    pairs = {}
-    while i>=0 and j>=0:
-        i_new, j_new = argmaxes[(i,j)]
-
-        #check if matches with null sentence
-        if i_new == i or j_new == j:
-            i,j = i_new, j_new
-            continue
-
-        if i_new == i-1:
-            part1 = i
-        elif i_new == i-2:
-            part1 = reverseIndex(i-1, i, set1)
-        if j_new == j-1:
-            part2 = j
-        elif j_new == j-2:
-            part2 = reverseIndex(j-1, j, set2)
-            
-        pairs[(part1, part2)] = maxes[i,j]
-        i,j = i_new, j_new
-
-    return pairs
-    
 def getMaximalMatching(set1, set2, scores, discourse=None, minLength=4, one2twoOnly=False):
     s = sorted(scores.items(), key=lambda x:x[1], reverse=True)
     newScores = {}
@@ -292,61 +151,4 @@ def getMaximalMatching(set1, set2, scores, discourse=None, minLength=4, one2twoO
             break
         
     return newScores
-
-def main(thresh=.24, interval=None, timeout=None):
-    starttime = time.time()
-
-    try:
-        discourse = loadDiscourse('markers')
-        causal = loadDiscourse('markers_causal')
-
-        with open('simplewiki.titles') as f:
-            titles = f.read().splitlines()
-
-        pairs = {}
-        for title in titles:
-            print(title)
-            en,simple = getParallelArticles(title) #, discourse)
-            scores = getMatching(en, simple)
-            new = getMaximalMatching(en, simple, scores, discourse) #causal)
-
-            for pair in new:
-                if new[pair] < thresh:
-                    continue
-
-                #dont care about pairs that are exactly the same .... or do I?
-                if new[pair] == 1:
-                    continue
-
-                i0, i1 = normalizeIndex(pair[0], en)
-                i2, i3 = normalizeIndex(pair[1], simple)
-                multisentence1 = getMultiSentence(en, i0, i1)
-                multisentence2 = getMultiSentence(simple, i2, i3)
-                pairs[(multisentence1, multisentence2)] = new[pair]
-
-            print(len(pairs))
-
-            if timeout is not None and time.time()-starttime > timeout*60:
-                raise Exception
-            if interval is not None and time.time()-starttime > interval*60:
-                writePairs(pairs)
-
-    except Exception as e:
-        raise e
-
-    return pairs
-
-def writePairs(pairs, outfile = 'pairs.out'):
-    if os.path.exists(outfile):
-        shutil.copy(outfile, outfile + '.backup')
-    with open(outfile, 'w') as f:
-        for pair in pairs:
-            print('{} , {} , {}'.format(pair[0].encode('utf-8'), 
-                                        pair[1].encode('utf-8'), 
-                                        pairs[pair]), 
-                  file=f)
-
-if __name__ == '__main__':
-    pairs = main(interval=1)
-    writePairs(pairs)
 
